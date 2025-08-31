67403bcfd707c7c3d138cf9a459ec613f41f4166
Infinity <igor74853@yandex.ru>
2025-08-31 13:04:06 +0500

    31.08.2025 13/00

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMeta {
+  readonly hot?: {
+    accept: (fn?: (module: any) => void) => void;
+    dispose: (fn: (data: any) => void) => void;
+    decline: () => void;
+    invalidate: () => void;
+    on: (event: string, fn: (data: any) => void) => void;
+  };
+}
